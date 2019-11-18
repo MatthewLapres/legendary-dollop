6a89d7638d91f3b4875924801d16d121d6673ce2
MatthewLapres <57884051+MatthewLapres@users.noreply.github.com>
2019-11-18 02:07:16 -0500

    Update GetInput.py
    Function will now write the two values to a csv file.

--- GetInput.py
+++ GetInput.py
@@ -1,2 +1,9 @@
+import csv #Used to create the formatted output file
+
 value1 = input("Please enter value 1: ")
 value2 = input("Please enter value 2: ")
+
+with open('pushup_values.csv', mode='w') as pushup_file:
+    employee_writer = csv.writer(pushup_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    employee_writer.writerow([value1, value2])
